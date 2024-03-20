d8995dc216f0277b1c406c7f0cc0044b79c45c52
Mikael Lindqvist <li.mikael@gmail.com>
2024-03-20 20:35:26 +0800

    qql

--- packages/katnip-quickmin/katnip-quickmin-isoq-wrapper.jsx
+++ packages/katnip-quickmin/katnip-quickmin-isoq-wrapper.jsx
@@ -1,12 +1,15 @@
 import {QuickminApiProvider} from "quickmin/use-api";
+import {QqlProvider} from "qql/react";
 import {useIsoContext} from "isoq";
 
 export default function({children}) {
     let iso=useIsoContext();
 
 	return (
-		<QuickminApiProvider url="/admin" fetch={iso.fetch}>
-			{children}
-		</QuickminApiProvider>
+		<QqlProvider url="/admin/_qql" fetch={iso.fetch}>
+			<QuickminApiProvider url="/admin" fetch={iso.fetch}>
+				{children}
+			</QuickminApiProvider>
+		</QqlProvider>
 	);
 }
