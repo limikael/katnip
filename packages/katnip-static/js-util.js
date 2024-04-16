80f7e3448b2e8dc728f62a9f6a6e99d0e5dd1089
Mikael Lindqvist <li.mikael@gmail.com>
2024-04-16 14:42:37 +0800

    static subpath

---
+++ packages/katnip-static/js-util.js
@@ -0,0 +1,19 @@
+export function splitPath(pathname) {
+	if (pathname===undefined)
+		throw new Error("Undefined pathname");
+
+	return pathname.split("/").filter(s=>s.length>0);
+}
+
+export function urlGetArgs(url) {
+	return splitPath(new URL(url).pathname);
+}
+
+export function urlGetParams(url) {
+	let u=new URL(url);
+	return Object.fromEntries(u.searchParams);
+}
+
+export function jsonEq(a,b) {
+	return (JSON.stringify(a)==JSON.stringify(b));
+}
--- packages/katnip-static/katnip-static-server-browser.js
+++ packages/katnip-static/katnip-static-server-browser.js
@@ -1,23 +1,32 @@
 import path from "path-browserify";
 import {exists} from "./fs-util.js";
 import {mimeTypes} from "./mime-types.js";
+import {urlGetArgs, splitPath, jsonEq} from "./js-util.js";
 
 fetch.priority=5;
 export async function fetch(req, ev) {
-    let u=new URL(req.url);
-    let cand=path.join(ev.cwd,ev.options.publicDir,u.pathname);
+    let urlParts=urlGetArgs(req.url);
+    let appPathnameParts=splitPath(ev.appPathname);
+    let prefixParts=urlParts.splice(0,appPathnameParts.length);
+    if (!jsonEq(prefixParts,appPathnameParts))
+        return;
 
-    if (await exists(ev.fsPromises,cand)) {
-        console.log("serving static: "+cand);
+    let cand=path.join(ev.cwd,ev.options.publicDir,...urlParts);
+    if (!await exists(ev.fsPromises,cand))
+        return;
 
-        let ext=path.extname(cand).replace(".","");
-        let headers=new Headers();
-        if (mimeTypes[ext])
-            headers.set("content-type",mimeTypes[ext]);
+    let stat=await ev.fsPromises.stat(cand);
+    if (!stat.isFile())
+        return;
 
-        let body=await ev.fsPromises.readFile(cand,"utf8");
-        return new Response(body,{
-            headers: headers
-        });
-    }
+    console.log("serving static: "+cand);
+    let ext=path.extname(cand).replace(".","");
+    let headers=new Headers();
+    if (mimeTypes[ext])
+        headers.set("content-type",mimeTypes[ext]);
+
+    let body=await ev.fsPromises.readFile(cand,"utf8");
+    return new Response(body,{
+        headers: headers
+    });
 }
