60c6470dd431e42d9b185777576894a6f9f13944
Mikael Lindqvist <li.mikael@gmail.com>
2024-04-18 08:53:03 +0800

    quickmin user management

--- packages/katnip-quickmin/katnip-quickmin-isoq-wrapper.jsx
+++ packages/katnip-quickmin/katnip-quickmin-isoq-wrapper.jsx
@@ -1,14 +1,18 @@
-import {QuickminApiProvider} from "quickmin/use-api";
+import {QuickminApiProvider, QuickminUserProvider} from "quickmin/use-api";
 import {QqlProvider} from "qql/react";
 import {useIsoContext} from "isoq";
 
-export default function({children}) {
+export default function({quickminUser, children}) {
     let iso=useIsoContext();
+
+    //console.log("quickminUser: ",quickminUser);
 
 	return (
 		<QqlProvider url="/admin/_qql" fetch={iso.fetch}>
 			<QuickminApiProvider url="/admin" fetch={iso.fetch}>
-				{children}
+				<QuickminUserProvider initialUser={quickminUser}>
+					{children}
+				</QuickminUserProvider>
 			</QuickminApiProvider>
 		</QqlProvider>
 	);
