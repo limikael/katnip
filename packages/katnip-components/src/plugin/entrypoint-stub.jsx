463b92696ef7c6ede918fd6496fbb8dd29b21f71
Mikael Lindqvist <li.mikael@gmail.com>
2024-05-16 20:33:11 +0800

    don't crash if page has no route

--- packages/katnip-components/src/plugin/entrypoint-stub.jsx
+++ packages/katnip-components/src/plugin/entrypoint-stub.jsx
@@ -3,6 +3,10 @@ $$CJSX_IMPORTS$$
 import {Route} from "isoq-router";
 import {ComponentLibraryProvider, useComponentLibrary} from "katnip-components";
 
+// NEED TO CHECK HERE IF Page HAS A ROUTE!!!
+
+
+
 export function PageRoute({Page}) {
 	let components=useComponentLibrary();
 
@@ -20,7 +24,8 @@ export function PageRoute({Page}) {
 }
 
 export default function() {
-	let pages=Object.values(CJSX_COMPONENTS).filter(c=>c.type=="Page");
+	let pages=Object.values(CJSX_COMPONENTS)
+		.filter(c=>(c.type=="Page" && c.route!==undefined));
 
 	return (<>
 		<ComponentLibraryProvider components={CJSX_COMPONENTS}>
