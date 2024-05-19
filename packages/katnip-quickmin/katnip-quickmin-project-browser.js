91ef6b2ae5f6b32111d717077d9433abb451d802
Mikael Lindqvist <li.mikael@gmail.com>
2024-05-19 13:21:25 +0800

    basic db

--- packages/katnip-quickmin/katnip-quickmin-project-browser.js
+++ packages/katnip-quickmin/katnip-quickmin-project-browser.js
@@ -4,15 +4,24 @@ import QuickminServer from "quickmin/server";
 import quickminQqlDriver from "quickmin/qql-driver";
 import * as TOML from "@ltd/j-toml";
 import {qqlDriverSqlJs} from "qql";
+import {exists} from "katnip";
 
 build.priority=15;
 export async function build(ev) {
-	console.log("quickmin browser build...");
+	//console.log("quickmin browser build...");
 	if (!ev.sql)
 		throw new Error("no db availble!!!");
 
 	let confFn=path.join(ev.cwd,"quickmin.yaml");
+	if (!await exists(confFn,{fs:ev.fs}))
+		throw new Error("No quickmin.yaml file found! If you don't want to use a database, disable the katnip-quickmin plugin");
+
 	let conf=parseYaml(await ev.fs.promises.readFile(confFn,"utf8"));
+	if (!conf)
+		conf={};
+
+	if (!conf.collections)
+		conf.collections={};
 
 	if (!conf.adminUser && !conf.adminPass) {
 		conf.adminUser="admin";
@@ -31,10 +40,35 @@ export async function build(ev) {
 	if (conf.apiPath!="admin")
 		throw new Error("Only api path 'admin' supported for now...");
 
-	conf.qqlDriver=qqlDriverSqlJs(ev.sql);
+	switch (ev.platform) {
+		case "browser":
+			conf.qqlDriver=qqlDriverSqlJs(ev.sql);
 
-	ev.data.quickminConf=conf;
+			ev.data.quickminConf=conf;
 
-	let server=new QuickminServer({...conf},[quickminQqlDriver]);
-	await server.sync({});
+			let server=new QuickminServer({...conf},[quickminQqlDriver]);
+			await server.sync({});
+			break;
+
+		case "workerd":
+			ev.data.quickminConf=conf;
+
+			//console.log("Migrating remote DB...")
+			let migrationConf={...conf};
+			migrationConf.qqlDriver=async (queries, returnType)=>{
+				if (returnType!="rows")
+					throw new Error("Only rows supported");
+
+				//console.log(queries);
+
+				return await ev.sqlExec(queries.join(";"));
+			}
+			let migrationServer=new QuickminServer(migrationConf,[quickminQqlDriver]);
+			await migrationServer.sync({});
+			break;
+
+		default:
+			throw new Error("Unknown platform: "+ev.platform);
+			break;
+	}
 }
