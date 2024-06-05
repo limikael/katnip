a4a0f1cdd53841059800808b93a3133c859c33a9
Mikael Lindqvist <li.mikael@gmail.com>
2024-06-05 13:07:24 +0800

    ver

--- packages/katnip-static/katnip-static-project-browser.js
+++ packages/katnip-static/katnip-static-project-browser.js
@@ -1,6 +1,6 @@
 build.priority=5;
 export function build(ev) {
-	//console.log("*********** static build...");
+	console.log("*********** static build...");
 	if (!ev.options.publicDir)
 		ev.options.publicDir="public";
 }
