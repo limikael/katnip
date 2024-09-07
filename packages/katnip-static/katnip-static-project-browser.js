736d4c8aff2caca1bee1f1f107dc94cd7f28023f
Mikael Lindqvist <li.mikael@gmail.com>
2024-09-07 09:19:36 +0800

    static content handler in browser url files

--- packages/katnip-static/katnip-static-project-browser.js
+++ packages/katnip-static/katnip-static-project-browser.js
@@ -1,6 +1,5 @@
 build.priority=5;
 export function build(ev) {
-	console.log("*********** static build...");
 	if (!ev.options.publicDir)
 		ev.options.publicDir="public";
 }
--- packages/katnip-static/katnip-static-server-browser.js
+++ packages/katnip-static/katnip-static-server-browser.js
@@ -2,6 +2,7 @@ import path from "path-browserify";
 import {exists} from "./fs-util.js";
 import {mimeTypes} from "./mime-types.js";
 import {urlGetArgs, splitPath, jsonEq} from "./js-util.js";
+import urlJoin from "url-join";
 
 fetch.priority=5;
 export async function fetch(req, ev) {
@@ -11,6 +12,16 @@ export async function fetch(req, ev) {
     if (!jsonEq(prefixParts,appPathnameParts))
         return;
 
+    let contentFileCand=path.join(ev.options.publicDir,...urlParts);
+    for (let contentFile of ev.contentFiles)
+        if (contentFile.name==contentFileCand) {
+            let parsedUrl=new URL(req.url);
+            let contentUrl=urlJoin(parsedUrl.origin,"admin/_content",contentFile.file);
+            //console.log("found content file: ",contentFile,"->",contentUrl);
+
+            return Response.redirect(contentUrl);
+        }
+
     let cand=path.join(ev.cwd,ev.options.publicDir,...urlParts);
     if (!await exists(ev.fsPromises,cand))
         return;
@@ -19,7 +30,7 @@ export async function fetch(req, ev) {
     if (!stat.isFile())
         return;
 
-    console.log("serving static: "+cand);
+    //console.log("serving static: "+cand);
     let ext=path.extname(cand).replace(".","");
     let headers=new Headers();
     if (mimeTypes[ext])
