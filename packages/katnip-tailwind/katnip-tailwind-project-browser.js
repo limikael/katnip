b09ce8a779ac7e6e71868904f74d8d1f57eb6896
Mikael Lindqvist <li.mikael@gmail.com>
2024-10-18 11:27:10 +0800

    build tailwind editor data

--- packages/katnip-tailwind/katnip-tailwind-project-browser.js
+++ packages/katnip-tailwind/katnip-tailwind-project-browser.js
@@ -3,9 +3,7 @@ import {findMatchingFiles, mkdirRecursive} from "./fs-util.js";
 import path from "path-browserify";
 import {resolveHookEntryPoints} from "katnip";
 
-export async function build(buildEv) {
-	console.log("Building tailwind, public="+buildEv.options.publicDir);
-
+async function buildTailwind(ev) {
 	let input=`
 @tailwind base;
 @tailwind components;
@@ -17,34 +15,34 @@ html, body, #isoq {
 }
 `;
 
-	/*let modulePaths=await resolveHookEntryPoints(buildEv.cwd,"isomain",{
-		fs: buildEv.fs,
-		keyword: "katnip-plugin"
-	});
-
-	if (!modulePaths.length) {
-		console.log("No tailwind css found, using default.")
-	}
-
-	else {
-		if (modulePaths.length!=1)
-			throw new Error("Expected 1 browser entry point, found "+modulePaths.length);
-
-		let mainParts=path.parse(modulePaths[0]);
-		let inputFn=path.join(mainParts.dir,mainParts.name+".css");
-		input=await buildEv.fsPromises.readFile(inputFn,"utf8");
-	}*/
-
-	//console.log("tailwind input: "+input);
-
-	let conf=(await buildEv.import(path.join(buildEv.cwd,"tailwind.config.js"))).default;
-	let fileNames=await findMatchingFiles(buildEv.fsPromises,buildEv.cwd,conf.content);
+	let conf=(await ev.import(path.join(ev.cwd,"tailwind.config.js"))).default;
+	let fileNames=await findMatchingFiles(ev.fs.promises,ev.cwd,conf.content);
 	let source="";
 	for (let fileName of fileNames)
-		source+=await buildEv.fsPromises.readFile(path.resolve(buildEv.cwd,fileName),"utf8");
+		source+=await ev.fs.promises.readFile(path.resolve(ev.cwd,fileName),"utf8");
 	//console.log("tailwind source files:",fileNames);
 
 	let output=await jitBrowserTailwindcss(input,source,conf);
+
+	return ({
+		css: output,
+		tailwindConfig: conf
+	});
+}
+
+export async function editorData(editorData, ev) {
+	//console.log("editor data in tailwind plugin...");
+	let artifact=await buildTailwind(ev);
+
+	editorData.css=artifact.css;
+	editorData.tailwindConfig=artifact.tailwindConfig;
+}
+
+export async function build(buildEv) {
+	//console.log("Building tailwind updated, public="+buildEv.options.publicDir);
+
+	let artifact=await buildTailwind(buildEv);
+	let output=artifact.css; //await buildTailwind(buildEv);
 
 	//console.log("tw build, publicDir="+buildEv.options.publicDir);
 
