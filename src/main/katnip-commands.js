6d9d7975860703f1ccc563c0af728c844e1be748
Mikael Lindqvist <li.mikael@gmail.com>
2025-07-19 11:28:05 +0800

    ver

--- src/main/katnip-commands.js
+++ src/main/katnip-commands.js
@@ -63,8 +63,13 @@ export async function katnipClean(options) {
 	await mikrokatClean(options);
 }
 
-export async function katnipCreateProvisionEnv(options) {
-	throw new Error("WIP");
+export async function katnipCreateProvisionEnv(options={}) {
+	options.cwd=await getEffectiveCwd(options.cwd);
+	options=await initOptions(options);
+	let project=new KatnipProject(options);
+	await project.load();
+
+	return await project.createProvisionEnv();
 }
 
 export async function katnipCreateTestEnv(options={}) {
